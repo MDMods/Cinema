96267df27a422179cb97e74a9b19e7a910408cbe
AshtonMemer <unofficialashton@gmail.com>
2023-05-30 03:40:43 -0500

    commit version 1.1.3

---
+++ Main.cs
@@ -0,0 +1,180 @@
+﻿using Assets.Scripts.Database;
+using Assets.Scripts.PeroTools.Commons;
+using CustomAlbums;
+using FormulaBase;
+using HarmonyLib;
+using MelonLoader;
+using System.IO;
+using System.Linq;
+using UnhollowerRuntimeLib;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace Cinema
+{
+    public class Main : MelonMod
+    {
+        public static bool GameStarted = false;
+        public static VideoPlayer Player;
+        public static CinemaInfo CinInfo;
+
+        public static void InitCamera()
+        {
+            string musicUid = DataHelper.selectedMusicUid;
+            if (!musicUid.StartsWith("999")) return;
+
+            Album currentAlbum = AlbumManager.LoadedAlbums.Values.First(a => a.Index.ToString() == musicUid.Remove(0, 4));
+            if (currentAlbum == null) return;
+
+            CinInfo = new CinemaInfo(currentAlbum);
+            if (CinInfo.jsonData == null) return;
+
+            byte[] videoBytes = currentAlbum.IsPackaged ? CinInfo.GetArchiveVideoBytes() : File.ReadAllBytes(CinInfo.FilePath);
+            File.WriteAllBytes(Application.persistentDataPath + "/cinema.mp4", videoBytes);
+
+
+            // Find the camera
+            Camera camToCopy = null;
+            foreach (var cam in Camera.allCameras)
+            {
+                if (cam.name == "Camera_3D")
+                {
+                    camToCopy = cam;
+                    break;
+                }
+            }
+            if (camToCopy == null) return;
+
+            // Create the new camera for cinema injection
+            var camera = Object.Instantiate(camToCopy);
+            camera.name = "Camera_Cinema";
+            camera.depth = -2;
+            camera.transform.position = new Vector3(0, 0, -9001);
+            camera.clearFlags = CameraClearFlags.SolidColor;
+            camera.orthographic = true;
+            camera.orthographicSize = Camera.main.orthographicSize;
+            camera.cullingMask = -1;
+            camera.backgroundColor = Color.black;
+            Camera.main.clearFlags = CameraClearFlags.Depth;
+
+            // Add video player to new camera
+            Player = camera.gameObject.AddComponent<VideoPlayer>();
+            Player.playOnAwake = false;
+            Player.targetCameraAlpha = CinInfo.Opacity;
+            Player.audioOutputMode = VideoAudioOutputMode.None;
+            Player.aspectRatio = VideoAspectRatio.FitOutside;
+            Player.url = Application.persistentDataPath + "/cinema.mp4";
+
+            SingletonMonoBehaviour<GameSceneContainer>.instance.sprLightness.color = new Color(0, 0, 0, 0);
+
+            HideAll(currentAlbum.Info.scene);
+        }
+
+        public static void HideAll(string sceneName)
+        {
+            Transform sceneObject = GameObject.Find("SceneObjectController").transform.Find(sceneName);
+            if (sceneObject == null) return;
+
+            sceneObject.gameObject.SetActive(false);
+
+            for (int i = 0; i < sceneObject.childCount; i++)
+            {
+                sceneObject.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(StageBattleComponent), "Load")]
+    internal static class Load_Patch
+    {
+        [HarmonyPostfix]
+        private static void Postfix()
+        {
+            Main.InitCamera();
+        }
+    }
+
+    [HarmonyPatch(typeof(SceneChangeController), "OnControllerStart")]
+    internal static class OnControllerStart_Patch
+    {
+        [HarmonyPrefix]
+        private static void Prefix()
+        {
+            if (Main.Player == null) return;
+            Main.HideAll($"scene_0{SceneChangeController.curScene}");
+        }
+
+        [HarmonyPostfix]
+        private static void Postfix()
+        {
+            if (Main.Player == null) return;
+            Main.HideAll($"scene_0{SceneChangeController.curScene}");
+        }
+    }
+
+    [HarmonyPatch(typeof(StageBattleComponent), "Pause")]
+    internal static class Pause_Patch
+    {
+        [HarmonyPrefix]
+        private static void Prefix()
+        {
+            if (Main.Player != null) Main.Player.Pause();
+        }
+    }
+
+    [HarmonyPatch(typeof(StageBattleComponent), "Resume")]
+    internal static class Resume_Patch
+    {
+        [HarmonyPrefix]
+        private static void Prefix()
+        {
+            if (!Main.GameStarted) return;
+            if (Main.Player != null) Main.Player.Play();
+        }
+    }
+
+    [HarmonyPatch(typeof(StageBattleComponent), "GameStart")]
+    internal static class GameStart_Patch
+    {
+        [HarmonyPostfix]
+        private static void Postfix()
+        {
+            if (Main.Player == null) return;
+            Main.GameStarted = true;
+            Main.Player.Play();
+        }
+    }
+
+    [HarmonyPatch(typeof(StageBattleComponent), "GameRestart")]
+    internal static class ReEnter_Patch
+    {
+        [HarmonyPrefix]
+        private static void Prefix()
+        {
+            Main.GameStarted = false;
+        }
+    }
+
+    [HarmonyPatch(typeof(StageBattleComponent), "Exit")]
+    internal static class Exit_Patch
+    {
+        [HarmonyPostfix]
+        private static void Postfix()
+        {
+            Main.Player = null;
+            Main.CinInfo = null;
+            Main.GameStarted = false;
+        }
+    }
+
+    [HarmonyPatch(typeof(FeverEffectManager), "ActivateFever")]
+    internal static class RemoveFeverBg_Patch
+    {
+        [HarmonyPostfix]
+        private static void Postfix(FeverEffectManager __instance)
+        {
+            if (Main.Player == null) return;
+            __instance.transform.Find("bg").Find("bg_S").gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Cinema")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cinema")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3b129f05-563f-4460-af9b-16e5d1c689b2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.3.0")]
+[assembly: AssemblyFileVersion("1.1.3.0")]
+[assembly: MelonInfo(typeof(Cinema.Main), "Cinema", "1.1.3", "AshtonMemer")]
+[assembly: MelonGame("PeroPeroGames", "MuseDash")]
+[assembly: MelonColor(System.ConsoleColor.DarkYellow)]
